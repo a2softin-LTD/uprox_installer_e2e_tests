4d790c80350ddb734d7beb5cd471d6650ca9482f
d.pinchuk <d.pinchuk@itvsystems.com.ua>
2024-11-14 10:40:28 +0200

    updated exist tests #11

---
+++ specs/sql/postgresql.spec.ts
@@ -0,0 +1,34 @@
+import { expect, test } from "@playwright/test";
+import { sequelize } from "../../db/db.config";
+import { GET_USERS} from "../../db/Query";
+import { Sequelize, QueryTypes } from "sequelize";
+
+test.describe('Get data from DEV DB', () => {
+
+    let devConnectionString: string;
+    let devSequelize: Sequelize;
+    let devConnection;
+
+    let allFileContents: any;
+
+    test.describe('Get data from users table', () => {
+
+        test('1. Get all users', async () => {
+            console.log('-------------------------------------------------------------------');
+            try {
+                await sequelize.authenticate();
+                console.log('Connection has been established successfully.');
+
+                const users = await sequelize.query(GET_USERS, { type: QueryTypes.SELECT });
+
+                console.log(users[0]['id']);
+
+            } catch (error) {
+                console.error('Unable to connect to the database:', error);
+            } finally {
+                await devConnection.release();
+            }
+        });
+
+    });
+});
