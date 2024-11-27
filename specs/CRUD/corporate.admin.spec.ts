eb6a21ca6e472bd1d057cb12419a2350502f5fa8
d.pinchuk <d.pinchuk@itvsystems.com.ua>
2024-11-27 12:57:59 +0200

    updated exist tests #23

--- specs/CRUD/corporate.admin.spec.ts
+++ specs/CRUD/corporate.admin.spec.ts
@@ -33,11 +33,15 @@ test.describe('Company Page tests', { tag: '@crud' },() => {
             expect(page.url()).toContain(URL_SUPPORT_SEARCH);
 
             await companyPage.companies.click();
+            await page.waitForLoadState('domcontentloaded');
+
             await companyPage.groupsOfCompanies.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(companyPage.pageTitle.filter({has:page.getByText(TITLE_GROUPS_OF_COMPANIES)})).toBeVisible();
 
             await companyPage.companyAddGroupButton.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(page.getByText(TEXT_ADDING_GROUPS_OF_COMPANIES)).toBeVisible();
 
@@ -45,34 +49,44 @@ test.describe('Company Page tests', { tag: '@crud' },() => {
             await companyPage.inputSecondField.fill(FAKER_EMAIL_FIRST);
 
             await companyPage.connectButton.click();
+            await page.waitForLoadState('domcontentloaded');
+
             await companyPage.connectButton.last().click()
-            await page.waitForTimeout(3000)
-            await companyPage.history.click({ timeout: 10000 });
+            await page.waitForLoadState('domcontentloaded');
+
+            await companyPage.history.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(page.getByText(TITLE_HISTORY_FOR_ALL_PANELS)).toBeVisible();
 
-            await companyPage.companies.click({ timeout: 10000 });
+            await companyPage.companies.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(page.getByText(TITLE_COMPANIES)).toBeVisible();
 
-            await companyPage.groupsOfCompanies.click({ timeout: 10000 });
+            await companyPage.groupsOfCompanies.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(companyPage.pageTitle.filter({has:page.getByText(TITLE_GROUPS_OF_COMPANIES)})).toBeVisible();
 
-            await page.waitForTimeout(3000);
+            await page.waitForLoadState('domcontentloaded');
             await page.reload();
-            await page.waitForTimeout(1000);
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(companyPage.pageTitle.filter({has:page.getByText(TITLE_GROUPS_OF_COMPANIES)})).toBeVisible();
             await expect(page.getByText(FAKER_NAME_OF_COMPANY_SECOND)).toBeVisible();
             await expect(page.getByText(FAKER_EMAIL_FIRST)).toBeVisible();
 
             await (page.getByText(FAKER_NAME_OF_COMPANY_SECOND)).click();
+            await page.waitForLoadState('domcontentloaded');
+
             await companyPage.deleteButton.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(page.getByText(TEXT_DELETING_GROUPS_OF_COMPANIES)).toBeVisible();
 
             await companyPage.deleteButton.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(companyPage.pageTitle.filter({has:page.getByText(TITLE_GROUPS_OF_COMPANIES)})).toBeVisible();
             await expect(page.getByText(FAKER_NAME_OF_COMPANY_SECOND)).not.toBeVisible();
@@ -90,11 +104,15 @@ test.describe('Company Page tests', { tag: '@crud' },() => {
             expect(page.url()).toContain(URL_SUPPORT_SEARCH);
 
             await companyPage.companies.click();
+            await page.waitForLoadState('domcontentloaded');
+
             await companyPage.groupsOfCompanies.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(companyPage.pageTitle.filter({has:page.getByText(TITLE_GROUPS_OF_COMPANIES)})).toBeVisible();
 
             await companyPage.companyAddGroupButton.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(page.getByText(TEXT_ADDING_GROUPS_OF_COMPANIES)).toBeVisible();
 
@@ -102,34 +120,44 @@ test.describe('Company Page tests', { tag: '@crud' },() => {
             await companyPage.inputSecondField.fill(FAKER_EMAIL_FIRST);
 
             await companyPage.connectButton.click();
+            await page.waitForLoadState('domcontentloaded');
+
             await companyPage.connectButton.last().click()
-            await page.waitForTimeout(3000)
-            await companyPage.history.click({ timeout: 10000});
+            await page.waitForLoadState('domcontentloaded');
+
+            await companyPage.history.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(page.getByText(TITLE_HISTORY_FOR_ALL_PANELS)).toBeVisible();
 
-            await companyPage.companies.click({ timeout: 10000 });
+            await companyPage.companies.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(page.getByText(TITLE_COMPANIES)).toBeVisible();
 
-            await companyPage.groupsOfCompanies.click({ timeout: 10000 });
+            await companyPage.groupsOfCompanies.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(companyPage.pageTitle.filter({has:page.getByText(TITLE_GROUPS_OF_COMPANIES)})).toBeVisible();
 
-            await page.waitForTimeout(3000);
+            await page.waitForLoadState('domcontentloaded');
             await page.reload();
-            await page.waitForTimeout(1000);
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(companyPage.pageTitle.filter({has:page.getByText(TITLE_GROUPS_OF_COMPANIES)})).toBeVisible();
             await expect(page.getByText(FAKER_NAME_OF_COMPANY_SECOND)).toBeVisible();
             await expect(page.getByText(FAKER_EMAIL_FIRST)).toBeVisible();
 
             await (page.getByText(FAKER_NAME_OF_COMPANY_SECOND)).click();
+            await page.waitForLoadState('domcontentloaded');
+
             await companyPage.deleteButton.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(page.getByText(TEXT_DELETING_GROUPS_OF_COMPANIES)).toBeVisible();
 
             await companyPage.deleteButton.click();
+            await page.waitForLoadState('domcontentloaded');
 
             await expect(companyPage.pageTitle.filter({has:page.getByText(TITLE_GROUPS_OF_COMPANIES)})).toBeVisible();
             await expect(page.getByText(FAKER_NAME_OF_COMPANY_SECOND)).not.toBeVisible();
